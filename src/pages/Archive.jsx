84dffd6d4679d546059dfca235a20212bea8027c
Danielle Lunas <daniell07720@gmail.com>
2024-11-13 16:58:10 +0800

    Fix: Bugs in Pagination

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast, Bounce } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 const Archive = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -15,68 +16,62 @@ const Archive = () => {
   const trashedTasksPerPage = 5;
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchTrashedTasks = async (
-      page = 1,
-      searchQuery = "",
-      status = ""
-    ) => {
-      setLoading(true);
-      try {
-        const token = localStorage.getItem("authToken");
+  const fetchTrashedTasks = async (page = 1, searchQuery = "", status = "") => {
+    setLoading(true);
+    setMessage("");
+    try {
+      const token = localStorage.getItem("authToken");
 
-        if (!token) {
-          toast.error("You must be logged in to view your trashed tasks.");
-          navigate("/login");
-          return;
-        }
-
-        const response = await axios.get(
-          // `http://localhost:8000/api/v1/task/trashed?page=${page}&per_page=${trashedTasksPerPage}`,
-          `http://localhost:8000/api/v1/task/trashed`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-            params: {
-              page: page,
-              per_page: trashedTasksPerPage,
-              query: searchQuery,
-              status: status,
-            },
-          }
-        );
-
-        // const data = response.data;
-        setTrashedTasks(
-          Array.isArray(response.data.data.data) ? response.data.data.data : []
-        );
-        // setTotalPages(data.meta?.last_page || 1);
-        // setCurrentPage(data.meta?.current_page || 1);
-        setCurrentPage(response.data.meta?.current_page || 1);
-        setTotalPages(response.data.meta?.last_page || 1);
-      } catch (error) {
-        console.error("Error fetching trashed tasks:", error);
-        // toast.error("Failed to load trashed tasks.");
-      } finally {
-        setLoading(false);
+      if (!token) {
+        toast.error("You must be logged in to view your trashed tasks.");
+        navigate("/login");
+        return;
       }
-    };
 
-    setCurrentPage(1);
-    fetchTrashedTasks(currentPage, searchQuery, status);
-  }, [currentPage, searchQuery, status]);
+      const response = await axios.get(
+        `http://localhost:8000/api/v1/task/trashed`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            page: page,
+            per_page: trashedTasksPerPage,
+            query: searchQuery,
+            status: status,
+          },
+        }
+      );
+
+      const tasks = response.data.data.data || [];
+      setTrashedTasks(tasks);
+      setCurrentPage(response.data.meta?.current_page || 1);
+      setTotalPages(response.data.meta?.last_page || 1);
+
+      if (tasks.length === 0) {
+        setMessage("No Task Found");
+      }
+    } catch (error) {
+      console.error("Error fetching trashed tasks:", error);
+      setMessage("No Tasks Found.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrashedTasks(currentPage, searchQuery);
+  }, [currentPage, searchQuery]);
 
   const handleSearch = async () => {
     if (searchQuery.trim() === "") {
-      fetchTrashedTasks(1);
+      fetchTrashedTasks(1, searchQuery);
       return;
     }
 
     try {
       const token = localStorage.getItem("authToken");
       const response = await axios.get(
-        // `http://localhost:8000/api/v1/trashed-search?query=${searchQuery}&status=${status}&page=1&per_page=${trashedTasksPerPage}`,
         `http://localhost:8000/api/v1/trashed-search`,
         {
           headers: {
@@ -85,20 +80,24 @@ const Archive = () => {
           params: {
             query: searchQuery,
             status: status.toLowerCase(),
-            // status: status.toUpperCase(),
             page: currentPage,
           },
         }
       );
 
       const data = response.data;
-      // console.log(response.data);
       setTrashedTasks(Array.isArray(data.data.data) ? data.data.data : []);
       setTotalPages(data.data.last_page);
       setCurrentPage(data.data.current_page);
+
+      if (!Array.isArray(data.data.data) || data.data.data.length === 0) {
+        setMessage("No Task Found");
+      } else {
+        setMessage("");
+      }
     } catch (error) {
       console.error("Error searching tasks:", error);
-      // setMessage("No Task Found");
+      setMessage("No Task Found");
     }
   };
 
@@ -235,8 +234,8 @@ const Archive = () => {
             </button>
           </>
         )}
-
         {/* Search bar */}
+        {/* // I will make this reusable soon //\ */}
         <input
           type="text"
           value={searchQuery}
@@ -244,12 +243,12 @@ const Archive = () => {
           placeholder="Search tasks..."
           className="border border-gray-300 rounded-md px-4 py-2 mr-2"
         />
-        <button
+        <Button
           onClick={handleSearch}
           className="bg-gray-500 text-white px-4 py-2 rounded-md"
         >
           Search
-        </button>
+        </Button>
       </div>
 
       {/* Task list */}
@@ -283,84 +282,99 @@ const Archive = () => {
                 </tr>
               </thead>
               <tbody>
-                {trashedTasks.map((task) => (
-                  <tr key={task.id}>
-                    <td className="border border-gray-300 px-4 py-2">
-                      <input
-                        type="checkbox"
-                        checked={selectedTasks.includes(task.id)}
-                        onChange={() => handleCheckboxChange(task.id)}
-                      />
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      {task.id}
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      {task.name}
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      {task.task}
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      {task.status}
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      <button
-                        onClick={() => restoreTask(task.id)}
-                        className="bg-blue-500 text-white text-xs md:text-md lg:text-lg px-4 py-2 rounded-md mr-2"
-                      >
-                        Restore
-                      </button>
-                      <button
-                        onClick={() => forceDeleteTask(task.id)}
-                        className="bg-red-500 text-white text-xs md:text-md lg:text-lg px-4 py-2 rounded-md"
-                      >
-                        Delete (Force)
-                      </button>
+                {trashedTasks.length === 0 ? (
+                  <tr>
+                    <td
+                      colSpan="6"
+                      className="text-center border border-gray-300 px-4 py-2"
+                    >
+                      No Task Found
                     </td>
                   </tr>
-                ))}
+                ) : (
+                  trashedTasks.map((task) => (
+                    <tr key={task.id}>
+                      <td className="border border-gray-300 px-4 py-2">
+                        <input
+                          type="checkbox"
+                          checked={selectedTasks.includes(task.id)}
+                          onChange={() => handleCheckboxChange(task.id)}
+                        />
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {task.id}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {task.name}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {task.task}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        {task.status}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        <Button
+                          onClick={() => restoreTask(task.id)}
+                          className="bg-blue-500 text-white text-xs md:text-md lg:text-lg px-4 py-2 rounded-md mr-2"
+                        >
+                          Restore
+                        </Button>
+                        <Button
+                          onClick={() => forceDeleteTask(task.id)}
+                          className="bg-red-500 text-white text-xs md:text-md lg:text-lg px-4 py-2 rounded-md"
+                        >
+                          Delete (Force)
+                        </Button>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </div>
 
           {/* Pagination */}
           <div className="flex justify-center mt-4">
-            <button
+            <Button
               onClick={() => handlePageChange(currentPage - 1)}
               className="px-4 py-2 mx-2 bg-black rounded-md text-white"
               disabled={currentPage === 1}
             >
               Previous
-            </button>
-            {Array.from({ length: totalPages }, (_, index) => index + 1).map(
-              (page) => (
-                <button
-                  key={page}
-                  onClick={() => handlePageChange(page)}
+            </Button>
+            {Array.from({ length: totalPages }, (_, index) => {
+              const pageNumber = index + 1;
+              return (
+                <Button
+                  key={pageNumber}
+                  onClick={() => handlePageChange(pageNumber)}
                   className={`px-4 py-2 mx-2 rounded-md ${
-                    page === currentPage
+                    currentPage === pageNumber
                       ? "bg-blue-500 text-white"
                       : "bg-green-500 text-black"
                   }`}
                 >
-                  {page}
-                </button>
-              )
-            )}
-            <button
+                  {pageNumber}
+                </Button>
+              );
+            })}
+            <Button
               onClick={() => handlePageChange(currentPage + 1)}
               className="px-4 py-2 mx-2 bg-black rounded-md text-white"
               disabled={currentPage === totalPages}
             >
               Next
-            </button>
+            </Button>
           </div>
         </>
       ) : (
-        <h1 className="text-5xl text-center text-gray-500 mt-4">
-          No archived tasks found
-        </h1>
+        !loading &&
+        !message && (
+          <h1 className="text-5xl text-center text-gray-500 mt-4">
+            No archived tasks found
+          </h1>
+        )
       )}
     </div>
   );
