45a76e1d089fbbff2806af6faa118da0d38e5c4c
Danielle Lunas <daniell07720@gmail.com>
2024-11-20 17:18:47 +0800

    fixed: responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
+import Footer from "../components/Footer";
 import { Link, useNavigate } from "react-router-dom";
 import { FaLock, FaUser } from "react-icons/fa";
 import Button from "../components/Button";
 import axios from "../components/axios";
 import { toast, Bounce } from "react-toastify";
+import { loginSchema } from "../utils/validations/UserSchema";
+import backgroundImg from "../assets/images/background-image.jpg";
+import Sonner from "../components/Sonner";
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const [message, setMessage] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -19,15 +23,22 @@ const Login = () => {
   }, [navigate]);
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (value === "") {
+      setErrors((prevErrors) => ({ ...prevErrors, [name]: "" }));
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setMessage("");
+    setErrors({ email: "", password: "" });
 
     try {
+      loginSchema.parse(formData);
+
       const response = await axios.post("/api/v1/login", formData);
 
       if (response.status === 200) {
@@ -50,59 +61,81 @@ const Login = () => {
         navigate("/dashboard");
       }
     } catch (error) {
-      setMessage("Invalid credentials.");
+      if (error.name === "ZodError") {
+        const newErrors = {};
+        error.errors.forEach((err) => {
+          newErrors[err.path[0]] = err.message;
+        });
+        setErrors(newErrors);
+      } else {
+        console.log(error);
+      }
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="bg-white wrapper flex flex-col justify-center items-center h-[100vh] gap-5">
-      <h1 className="font-poppins text-2xl p-3 font-bold">Task Management</h1>
-      <form
-        className="flex flex-col gap-4 items-center border-2 border-white bg-[#D72323] px-5 py-10 rounded-md w-[360px]"
-        onSubmit={handleSubmit}
+    <div className="min-h-screen flex flex-col">
+      <div
+        className="bg-white bg-cover bg-bottom flex justify-center items-center flex-1"
+        style={{ backgroundImage: `url(${backgroundImg})` }}
       >
-        <h1 className="font-poppins p-3 font-bold">Sign In</h1>
-        <div className="flex items-center gap-1 w-full">
-          <FaUser className="icon" />
-          <input
-            className="py-1 pl-1 rounded-md w-full"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email Address"
-            required
-          />
+        <div className="bg-gray-100 bg-opacity-90 px-5 py-10 rounded-md shadow-md w-[90%] max-w-[400px] md:max-w-[350px] lg:w-[30%]">
+          <h1 className="font-poppins text-2xl font-bold text-center mb-5">
+            Task Management
+          </h1>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <h2 className="font-poppins font-bold text-xl text-center">
+              Sign In
+            </h2>
+            <div className="flex items-center gap-2">
+              <FaUser className="text-black" />
+              <input
+                className="flex-1 py-2 px-3 border rounded-md"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Email Address"
+                required
+              />
+            </div>
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+            <div className="flex items-center gap-2">
+              <FaLock className="text-black" />
+              <input
+                className="flex-1 py-2 px-3 border rounded-md"
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Password"
+                required
+              />
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm">{errors.password}</p>
+            )}
+            <Button
+              className="bg-blue-400 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-blue-600 transition"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? <Sonner /> : "Log In"}{" "}
+            </Button>
+            <p className="text-center text-sm mt-4">
+              Don't have an account?{" "}
+              <Link className="text-blue-500 hover:underline" to={"/sign-up"}>
+                Sign Up
+              </Link>
+            </p>
+          </form>
         </div>
-        <div className="flex items-center gap-1 w-full">
-          <FaLock className="icon" />
-          <input
-            className="py-1 pl-1 rounded-md w-full"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="Password"
-            required
-          />
-        </div>
-        <Button
-          className={"rounded-full px-6 py-1"}
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Logging in..." : "Log In"}
-        </Button>
-        {message && <p className="text-white mt-2">{message}</p>}
-        <p className="font-poppins">
-          Don't have an account?{" "}
-          <Link className="hover:underline" to={"/sign-up"}>
-            Sign Up
-          </Link>
-        </p>
-      </form>
+      </div>
+
+      <Footer />
     </div>
   );
 };
