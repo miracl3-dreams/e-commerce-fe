a5f87217c93bf2ae2aa2ae65837c7998ee1a7264
Danielle <daniell07720@gmail.com>
2025-01-23 16:12:54 +0800

    Fix: route for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
-import Dashboard from "./pages/Dashboard/dashboard";
-import Tasks from "./pages/Tasks/tasks";
-import Posts from "./pages/Posts/posts";
-import Contact from "./pages/Contact/contact";
-import Login from "./pages/Login/login";
-import SignUp from "./pages/Signup/signup";
-import Settings from "./pages/Settings/settings";
-import AuthRoute from "./routes/AuthRoute";
+import NavigationBar from "@/components/NavigationBar";  
+import Dashboard from "@/pages/Dashboard/dashboard";  
+import Tasks from "@/pages/Tasks/tasks";  
+import Posts from "@/pages/Posts/posts";  
+import Contact from "@/pages/Contact/contact";  
+import Login from "@/pages/Login/login";  
+import SignUp from "@/pages/Signup/signup";  
+import Settings from "@/pages/Settings/settings";  
+import AuthRoute from "@/routes/AuthRoute";  
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Archive from "./pages/Archive/archive.jsx";
-import Test from "./pages/test/test";
+import Archive from "@/pages/Archive/archive.jsx"; 
+import Test from "@/pages/test/test";  
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
